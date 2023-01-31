4de006eaa5b2d9f9a42f581887f1ff5b1d8e8d3a
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-01-31 22:09:33 +0300

    delete exiting all file

--- src/lib/contexts/StoreContext.type.ts
+++
@@ -1,6 +0,0 @@
-import { StoreDataType, StoreAction } from "../states/storeReducer/storeReducer.type";
-
-export type StoreContextType = {
-    state: StoreDataType;
-    dispatch: React.Dispatch<StoreAction>;
-};
