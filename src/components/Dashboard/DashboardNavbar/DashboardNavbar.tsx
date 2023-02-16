2c9f423b5aa5993fcffce47d9e4b5498fbde874a
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-02-16 13:32:41 +0300

    notification

--- src/components/Dashboard/DashboardNavbar/DashboardNavbar.tsx
+++ src/components/Dashboard/DashboardNavbar/DashboardNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { AiFillSetting } from "react-icons/ai";
 import { MdLogout } from "react-icons/md";
@@ -7,8 +7,10 @@ import { useRouter } from "next/router";
 import { StoreActionType } from "@/lib/states/storeReducer/storeReducer.type";
 import Link from "next/link";
 import Image from "next/image";
+import Notifications from "./Notifications/Notifications";
 
 const DashboardNavbar = () => {
+    const [showNotification, setShowNotification] = useState(false);
     const { state, logOut, dispatch } = useStoreContext();
     const { user } = state;
     const router = useRouter();
@@ -68,26 +70,34 @@ const DashboardNavbar = () => {
                 </form>
 
                 <div className="flex items-center justify-end w-1/4">
-                    <button className="btn btn-ghost btn-circle relative">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-7 w-7 text-white"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                            />
-                        </svg>
-                        <span className="h-3 w-3 absolute top-1">
-                            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-600 opacity-75"></span>
-                            <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
-                        </span>
-                    </button>
+                    <div
+                        className="relative"
+                        onMouseOver={() => setShowNotification(true)}
+                        onMouseLeave={() => setShowNotification(false)}
+                    >
+                        <button className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-7 w-7 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                />
+                            </svg>
+                            <span className="h-3 w-3 absolute top-1">
+                                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-600 opacity-75"></span>
+                                <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
+                            </span>
+                        </button>
+                        <Notifications showNotification={showNotification} />
+                    </div>
+
                     <div className="dropdown dropdown-end">
                         <label
                             tabIndex={0}
