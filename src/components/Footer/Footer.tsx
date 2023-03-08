fb524a2d617c92dde79f1d0c2be74d19d2b9e78e
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-03-08 07:10:47 +0300

    done responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = () => {
                                 <input
                                     type="text"
                                     placeholder="username@site.com"
-                                    className="input input-bordered w-full pr-16"
+                                    className="input input-bordered w-full pr-28"
                                 />
                                 <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">
                                     Subscribe
