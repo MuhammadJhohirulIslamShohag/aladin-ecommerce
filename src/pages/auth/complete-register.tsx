20e01af1a666bd5460123560f4694ff396be4576
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-03-09 00:00:39 +0300

    change axios

--- src/pages/auth/complete-register.tsx
+++ src/pages/auth/complete-register.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { updatePassword } from "firebase/auth";
