ada37f72c2b1e608c97419489a6fe2d2552565e4
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2024-06-05 17:48:39 +0600

    [done] change next config file setting

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://54.204.167.67/api/v1/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
