6a56fb605fa20cfcf0469a675bed2f1813dded5e
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2024-06-05 15:13:48 +0600

    [done] deploy to vercel

--- v2/src/config/envConfig.ts
+++ v2/src/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const config = {
-    baseURL: process.env.NEXT_PUBLIC_SERVER_API || "http://54.204.167.67/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_SERVER_API || "/api",
     stripeKey: process.env.NEXT_PUBLIC_STRIPE_KEY || ""
 };
 
